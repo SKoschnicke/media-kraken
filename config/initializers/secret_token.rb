2ae0ad54d5a27b1a73b4a60f8f5d6695234541fa
SKoschnicke <s.koschnicke@gfxpro.com>
2012-06-26 12:40:37 +0200

    created new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediaKraken::Application.config.secret_token = 'adf60cd2f47e62a9c60934ce3c1fa20865e72fc668ab0cd8d23899abd021ecaafdf3e5dd065ecd97d3a97a5a193b6cc9fdcbf3c42386f1e33f618632c37632f7'
